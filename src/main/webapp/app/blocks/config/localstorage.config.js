4dc6ad13b65607bae7b5b97a601078fd50aa7eff
Thái Nguyễn <nguyenquocthai.100797@outlook.com.vn>
2018-06-13 09:12:00 +0700

    Initial application generated by JHipster-4.14.4

---
+++ src/main/webapp/app/blocks/config/localstorage.config.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('paResApp')
+        .config(localStorageConfig);
+
+    localStorageConfig.$inject = ['$localStorageProvider', '$sessionStorageProvider'];
+
+    function localStorageConfig($localStorageProvider, $sessionStorageProvider) {
+        $localStorageProvider.setKeyPrefix('jhi-');
+        $sessionStorageProvider.setKeyPrefix('jhi-');
+    }
+})();
---
+++ src/main/webapp/app/components/form/pagination.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('paResApp')
+        .constant('paginationConstants', {
+            'itemsPerPage': 20
+        });
+})();
